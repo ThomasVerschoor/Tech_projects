d776ace9f280c4bd25711b0535f9e324fdc3a7ef
Thomas Verschoor <thomas.verschoor29@gmail.com>
2022-10-18 22:03:14 +0200

    added table containing projects out of api call

--- script.js
+++ script.js
@@ -8,5 +8,22 @@ fetch("https://thomasverschoor.github.io/TechProjectsCMS/")
   })
   .then(data => {
     console.log(data);
+    writeToWebPage(data)
   })
   .catch((error) => console.error("FETCH ERROR:", error));
+
+
+function writeToWebPage(data) {
+
+  var container = document.getElementById("projectsList");
+
+  projects = data["projects"]
+  for (elem of projects)
+        {
+            // Print the current element
+            console.log(elem["title"]);
+            let tableBody = document.getElementById("tableBody");
+            tableBody.innerHTML += '<tr><td>' + elem["title"] + '</td><td>' + elem["dateCreated"] + '</td></tr>';
+        }
+
+}
