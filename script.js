e43075f6916dfb91251e404e40d8bb697f8de96d
Thomas Verschoor <thomas.verschoor29@gmail.com>
2022-10-28 22:47:31 +0200

    added component tutorials section on index webpage

--- script.js
+++ script.js
@@ -6,7 +6,7 @@ function writeToWebPage(data)
 
   projects = data["projects"]
 
-
+    // projects on webpage
     let project = document.getElementById("projectsOverview")
 
     let projectContent = '<div class="row">'
@@ -27,6 +27,31 @@ function writeToWebPage(data)
 
 
     project.innerHTML = projectContent
+
+
+    // loading of component data
+    components = data["components"]
+
+    // components on webpage
+    let component = document.getElementById("componentsOverview")
+
+    let compContent = '<div class="row">'
+
+    for (comp of components)
+    {
+      let hrefComponent ="component.html?="+comp["componentID"]
+      compContent += '<div class="column"><img src="'+comp["imageLink"]+'" alt="Nature" style="width:100%">'
+      compContent +='<a href='+hrefComponent+'>'+comp["title"]+'</a>'
+      compContent +='</div>'
+
+    }
+
+    compContent +='</div>'
+
+
+    component.innerHTML = compContent
+
+
 }
 
 //fetches all data
