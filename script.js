b0b40a81582d9a59b21c22fefd47e8243c32f191
Thomas Verschoor <thomas.verschoor29@gmail.com>
2022-10-28 22:02:13 +0200

    deleted table with projects

--- script.js
+++ script.js
@@ -5,35 +5,28 @@ function writeToWebPage(data)
   var container = document.getElementById("projectsList");
 
   projects = data["projects"]
-  for (elem of projects)
-        {
-            let tableBody = document.getElementById("tableBody");
-            let hrefAuthor = "author.html?="+elem["author"]
-            let hrefProject = "project.html?="+elem["projectID"]
-            tableBody.innerHTML += '<tr><td>' +'<a href='+hrefProject+'>'+elem["title"]+'</a>' + '</td><td>' + elem["dateCreated"] + '</td><td>' +'<a href='+hrefAuthor+'>'+elem["author"]+'</a>' +'</td></tr>' ;
-
-        }
-
-        let project = document.getElementById("projectsOverview")
-
-        let projectContent = '<div class="row">'
-
-        for (elem of projects)
-        {
-          let hrefProject = "project.html?="+elem["projectID"]
-          let hrefAuthor = "author.html?="+elem["author"]
-          projectContent += '<div class="column"><img src="'+elem["imageLink"]+'" alt="Nature" style="width:100%">'
-          projectContent +='<a href='+hrefProject+'>'+elem["title"]+'</a>'
-          projectContent +='<br>'
-          projectContent +='by <a href='+hrefAuthor+'>'+elem["author"]+'</a>'
-          projectContent +='</div>'
-
-        }
-
-        projectContent +='</div>'
 
 
-        project.innerHTML = projectContent
+    let project = document.getElementById("projectsOverview")
+
+    let projectContent = '<div class="row">'
+
+    for (elem of projects)
+    {
+      let hrefProject = "project.html?="+elem["projectID"]
+      let hrefAuthor = "author.html?="+elem["author"]
+      projectContent += '<div class="column"><img src="'+elem["imageLink"]+'" alt="Nature" style="width:100%">'
+      projectContent +='<a href='+hrefProject+'>'+elem["title"]+'</a>'
+      projectContent +='<br>'
+      projectContent +='by <a href='+hrefAuthor+'>'+elem["author"]+'</a>'
+      projectContent +='</div>'
+
+    }
+
+    projectContent +='</div>'
+
+
+    project.innerHTML = projectContent
 }
 
 //fetches all data
